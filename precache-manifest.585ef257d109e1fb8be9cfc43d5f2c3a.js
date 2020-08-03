89f0255498145f61ec5ffdf8b564d72d5f19c9af
Jinyoung <roghabo@gmail.com>
2020-08-03 15:38:12 +0900

    Updates

---
+++ precache-manifest.585ef257d109e1fb8be9cfc43d5f2c3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cf17dfc2794097205bc",
+    "url": "/nomflix/static/js/main.8cf17dfc.chunk.js"
+  },
+  {
+    "revision": "97602d06c2fa05a808d5",
+    "url": "/nomflix/static/js/1.97602d06.chunk.js"
+  },
+  {
+    "revision": "621cdbff6339be2fca9f",
+    "url": "/nomflix/static/js/runtime~main.621cdbff.js"
+  },
+  {
+    "revision": "fee281b9e48d3ab7690a3b35efc6d1f1",
+    "url": "/nomflix/static/media/noPosterSmall.fee281b9.png"
+  },
+  {
+    "revision": "fb819797ebef849658cdf93b28dda1cd",
+    "url": "/nomflix/index.html"
+  }
+];
